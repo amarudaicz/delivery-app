b71e31e49b6f0b15d9be586122e3c1a7a84b6fae
amaru <amarudaicz10@gmail.com>
2023-03-11 11:10:50 -0300

    fvg

--- src/app/home/components/newselter-box/newselter-box.component.ts
+++ src/app/home/components/newselter-box/newselter-box.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 import { ThemesService } from 'src/app/services/themes/themes.service';
 
 @Component({
@@ -6,8 +8,41 @@ import { ThemesService } from 'src/app/services/themes/themes.service';
   templateUrl: './newselter-box.component.html',
   styleUrls: ['./newselter-box.component.scss']
 })
-export class NewselterBoxComponent {
-  constructor(public theme:ThemesService){
+export class NewselterBoxComponent implements OnInit {
+  constructor(
+    public theme:ThemesService,
+    private swUpdate: SwUpdate,
+    private swPush: SwPush,
+    private snackBar: MatSnackBar){
 
   }
+
+  deferredPrompt: any;
+
+
+  ngOnInit(): void {
+    window.addEventListener('beforeinstallprompt', (event: any) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = event;
+      // Update UI to notify the user they can add to home screen
+    });
+  }
+
+  install(): void {
+    // Show the install prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      this.deferredPrompt = null;
+    });
+  }
+
+
 }
