0bcf4971984d3c1830a28fdce8a7fe85f54a1e34
amarudaicz <amarudaicz10@gmail.com>
2023-10-09 23:31:24 -0300

    stable

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production:true,
-    host:'http://localhost:3001/',
+    host:'149.50.132.136',
     // host:'https://delivery-app-backend-dev.fl0.io/',
     cloudinary_cloud:'diyorb8ka'
 }
