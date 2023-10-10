be65eaeecfe1ee97310dee903ccd2a2a5f27c589
amarudaicz <amarudaicz10@gmail.com>
2023-10-10 19:58:09 -0300

    host ip

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production:true,
-    host:'149.50.132.136',
+    host:'149.50.132.136', 
     // host:'https://delivery-app-backend-dev.fl0.io/',
     cloudinary_cloud:'diyorb8ka'
 }
