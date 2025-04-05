aad9c2d472e2fe7b570789861982c0aa43a2b843
amarudaicz <amarudaicz10@gmail.com>
2025-04-05 13:10:56 -0300

    REMOVE: Install app, home, new cloudinary

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     production:true,
     host:'https://site--deli--grqgr7rz9qhl.code.run/', 
     // host:'https://delivery-app-backend-dev-kdrs.1.us-1.fl0.io/',
-    cloudinary_cloud:'dnvycredk'
+    cloudinary_cloud:'dwklrqceu'
 }
 
